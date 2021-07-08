820716b3cd7724cc7981fcb24155fd23d2225c92
sr1k1 <86798574+sr1k1@users.noreply.github.com>
2021-07-07 22:36:14 -0400

    Add files via upload

--- Three Box Ocean Model.py
+++ Three Box Ocean Model.py
@@ -23,17 +23,21 @@ rho_0 = 1024.5 #Units of kg per cubic meter, density of water
 ## The following functions abstract the process of creating the transport model
 ## (using the ODEs we have generalized to the system).
 
-def create_transport_model(C_1, C_2, C_3, dt_in_years, end_time):
+def create_transport_model(C_1, C_2, C_3, dt_in_years, end_time, title, element_symbol, lambda_1 = 0, lambda_2 = 0):
     """
     Uses first order ODEs to characterize the time dependence of the concentration(s)
     in the three boxes (of fixed dimension).
     
     Parameters:
     -----------
-        C_1, C_2, C_3: Initial concentrations in the three boxes (int), 
+        C_1, C_2, C_3: Initial concentrations in the three boxes (float), 
             quantities in mols per cubic meter.
         dt_in_years: float, length of time step
         end_time: int, number of years for which we want to process the simulation
+        title: str, title to be given to graph.
+        element_symbol: element to be considered (e.g. N for nitrogen) (str)
+        lambda_1, lambda_2: float, rate coefficient relating consumption and export
+            (s**-1). Set to 0 by default unless specified. 
 
     Returns
     -------
@@ -41,6 +45,7 @@ def create_transport_model(C_1, C_2, C_3, dt_in_years, end_time):
                            (array of the concentrations over time for C_1, \
                             array of the concentrations over time for C_2, \
                                 array of the concentrations over time for C_3))
+        Plots graph with this information (not returned)
     """
 
     ## Establishing Dimensions of Three Boxes (all values have units of meters)
@@ -94,7 +99,7 @@ def create_transport_model(C_1, C_2, C_3, dt_in_years, end_time):
         rates and the concentrations at the given times.
     
         """
-        return (psi*(C_3 - C_1) + k_31*(C_3 - C_1) + k_21*(C_2 - C_1))/vol_1
+        return (psi*(C_3 - C_1) + k_31*(C_3 - C_1) + k_21*(C_2 - C_1))/vol_1 - lambda_1*C_1
     
     def dC_2_over_dt():
         """
@@ -110,7 +115,7 @@ def create_transport_model(C_1, C_2, C_3, dt_in_years, end_time):
         rates and the concentrations at the given times.
     
         """
-        return (psi*(C_1 - C_2) + k_12*(C_1 - C_2) + k_32*(C_3 - C_2))/vol_2
+        return (psi*(C_1 - C_2) + k_12*(C_1 - C_2) + k_32*(C_3 - C_2))/vol_2 - lambda_2*C_2
     
     def dC_3_over_dt():
         """
@@ -126,7 +131,8 @@ def create_transport_model(C_1, C_2, C_3, dt_in_years, end_time):
         rates and the concentrations at the given times.
     
         """
-        return (psi*(C_2 - C_3) + k_23*(C_2 - C_3) + k_13*(C_1 - C_3))/vol_3
+        return (psi*(C_2 - C_3) + k_23*(C_2 - C_3) + k_13*(C_1 - C_3))/vol_3 + \
+            (lambda_1*C_1*vol_1 + lambda_2*C_2*vol_2)/vol_3
     
     
     ### Create arrays where the first array depicts the x-axis (time steps) and the three other
@@ -178,47 +184,70 @@ def create_transport_model(C_1, C_2, C_3, dt_in_years, end_time):
     C_3_array = np.array(C_3_list)
         # Once the above is complete, we now have three arrays depicting concentrations of C_1, C_2, and C_3
         # over time. 
+    
+    def plot_concentrations(title, element_symbol, time_axis_array, array_of_C_1, array_of_C_2, array_of_C_3):
+        """
+        Plots the concentrations of material in the three boxes and their
+        change over time
+    
+        Parameters:
+        -----------
+            title: an str object titling the graph
+            time_array: array of ints representing the time scale of the graph.
+            array_of_C_1: array of numbers detailing levels of C_1 over time.
+            array_of_C_2 and array_of_C_3: as detailed above.
+        
+        Returns:
+        -------
+            None. Plots graph with above information.
+    
+        """
+        plt.title(f'{title}')
+        plt.xlabel('Time (years)')
+        plt.ylabel(f'Concentration (mol {element_symbol}_i per cubic meter)')
+        plt.plot(time_axis_array, array_of_C_1, 'r', label = f'{element_symbol}_1')
+        plt.plot(time_axis_array, array_of_C_2, 'm', label = f'{element_symbol}_2')
+        plt.plot(time_axis_array, array_of_C_3, 'b', label = f'{element_symbol}_3')
+        plt.legend(loc = 'best')
+        plt.show()
+        
+        
+    plot_concentrations(title, element_symbol, time_axis_array, \
+                    C_1_array, C_2_array, C_3_array)
+        # Plotting the concentrations and how they change over time. 
     return (time_axis_array, (C_1_array, C_2_array, C_3_array))
     
 
-def plot_concentrations(title, time_axis_array, array_of_C_1, array_of_C_2, array_of_C_3):
-    """
-    Plots the concentrations of material in the three boxes and their
-    change over time
-
-    Parameters:
-    -----------
-        title: an str object titling the graph
-        time_array: array of ints representing the time scale of the graph.
-        array_of_C_1: array of numbers detailing levels of C_1 over time.
-        array_of_C_2 and array_of_C_3: as detailed above.
-    
-    Returns:
-    -------
-        None. Plots graph with above information.
-
-    """
-    plt.title(f'{title}')
-    plt.xlabel('Time (years)')
-    plt.ylabel('Concentration (mol C_i per cubic meter)')
-    plt.plot(time_axis_array, array_of_C_1, 'r', label = 'C_1')
-    plt.plot(time_axis_array, array_of_C_2, 'm', label = 'C_2')
-    plt.plot(time_axis_array, array_of_C_3, 'b', label = 'C_3')
-    plt.legend(loc = 'best')
-    plt.show()
-
 ### --------------------------------------------------------------------------------
-## The following section calls the above functions for plotting purposes.
+### The following section calls the above functions for plotting purposes.
 
 
-# Part 1: Creating Transport Model for Generic Concentration
-transport_model_info = create_transport_model(0.1, 0.1, 1.0, 1, 100)
-    # The tuple above is of the form (time_array, (C1_array, C2_array, C3_array)).
-plot_concentrations('Concentrations of C_1, C_2, and C_3', transport_model_info[0], \
-                    transport_model_info[1][0], transport_model_info[1][1], \
-                        transport_model_info[1][2])
+## Part 1: Creating Transport Model for Generic Concentration
+
+# Concentrations of 1.0 for all three boxes:
     
+transport_model_info = create_transport_model(1.0, 1.0, 1.0, 1, 100, 'Concentrations of C_1, C_2, and C_3, initially all 1.0,' \
+                                              + ' dt = 1', 'C')
+    # The tuple above is of the form (time_array, (C1_array, C2_array, C3_array)).
+
+# Concentrations of 0.1 for boxes 1 and 2, and 1.0 for box 3
+
+transport_model_plot = create_transport_model(0.1, 0.1, 1.0, 1, 100, 'Concentrations of C_1 = 0.1, C_2 = 0.1, and C_3 = 1.0 initially,' \
+                                              + ' dt = 1', 'C')
+
+# -----------------------------------------------------------------------------------------------------------------------------------------
 # Part 2: DIC and air-sea exchange of CO2. 
 DIC_1_to_3 = 2000.0*rho_0*10**(-6) 
     # After proper conversions, the above essentially represents C_1 to C_3, with units of 
     # mol per cubic meter. 
+
+### To be continued (maybe).
+
+# -------------------------------------------------------------------------------------------------------------------
+# Part 3: Representing Soft Tissue Pump
+
+N_1_to_3 = 30*rho_0*10**(-6) 
+    # After proper conversions, the above essentially represents N_1 to N_3, with units of 
+    # mol per cubic meter.
+    
+transport_model_info = create_transport_model(N_1_to_3, N_1_to_3, N_1_to_3, 0.001, 10, 'Concentrations of N_1, N_2, and N_3 w/ exports, dt = 0.001', 'N',  3*10**-7, 3*10**-8)
